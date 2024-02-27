2884f9a555744f50917177177ce5931aadbe3ba5
Hannah Clausen <hannahdk00@gmail.com>
2024-02-27 11:06:59 +0100

    arbejde med products uden api

--- lemonade_webshop/src/App.js
+++ lemonade_webshop/src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Navbar } from "./components/navbar";
+import {Shop} from "./pages/shop/shop";
+import {Cart} from "./pages/cart/cart";
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'http://www.google.com';
 
   return (
     <div className="App">
-      <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
-      </div>
+      {" "}
+      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
