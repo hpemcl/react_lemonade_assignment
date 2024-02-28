23a1806395dee3acdaed33292b1fff57fe6c512a
hannahbanana <hannahdk00@gmail.com>
2024-02-28 17:46:51 +0100

    product and price

--- lemonade_webshop/src/pages/shop/product.jsx
+++ lemonade_webshop/src/pages/shop/product.jsx
@@ -4,6 +4,16 @@ export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
 
     return (
-    <div> {productName} </div>
+    <div className="product"> 
+    
+    <img src="{productImage}" />
+        <div className="description">
+            <p> 
+                <b> {productName} </b>  
+            </p>
+            <p> ${price} </p>
+        </div>
+
+    </div>
     );
 };
