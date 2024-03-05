1904eed7db92fe94c206025c8a24c07b4c1e9222
hannahbanana <hannahdk00@gmail.com>
2024-03-05 12:52:40 +0100

    begyndelsen af stylingen på homepage

--- lemonade_webshop/src/pages/home/home.jsx
+++ lemonade_webshop/src/pages/home/home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./home.css";
+import billedeLogo from "../../assets/billeder/logo.png";
+
 
 export const Home = () => {
   
     return (
-      <div>home</div>
+      <div className="first-section">
+        <div className="title">
+          <h1 className="first-title"> Welcome to the lemonade shop</h1>
+        </div>
+        
+        <div className="firstLogo">
+          <img className="first-section-logo" src={billedeLogo} alt="Logo" />
+        </div>
+        <h1>Nico was here</h1>
+      </div>
 
     );
   };
